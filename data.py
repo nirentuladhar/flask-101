a89a779d63ccaee2299aa3ec80f218b1ac9a4d9c
Niren <nirentuladhar@gmail.com>
2018-01-07 01:09:10 +1100

    initial setup and user registration

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            "id": 1,
+            "title": "Article One",
+            "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ipsum animi dolore in reiciendis similique repellat. Consequuntur et eos quis at in nobis eveniet, sapiente veritatis pariatur, saepe ratione quod.",
+            "author": "Ben Thompson",
+            "create_date": "04-25-2017"
+        },
+        {
+            "id": 2,
+            "title": "Article Two",
+            "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ipsum animi dolore in reiciendis similique repellat. Consequuntur et eos quis at in nobis eveniet, sapiente veritatis pariatur, saepe ratione quod.",
+            "author": "Jane Doe",
+            "create_date": "04-25-2017"
+        },
+        {
+            "id": 3,
+            "title": "Article Three",
+            "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ipsum animi dolore in reiciendis similique repellat. Consequuntur et eos quis at in nobis eveniet, sapiente veritatis pariatur, saepe ratione quod.",
+            "author": "John Doe",
+            "create_date": "04-25-2017"
+        }
+    ]
+    return articles
